74e43ffd2e9b4d5c9f914eb848069fe3c1158801
Florent Menus <fmenus@shin-agency.com>
2025-12-05 17:32:20 +0100

    feat: route to promote an user to admin & a command to create an admin

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
+import { UserRole } from "@prisma/client";
 
 export const auth = (req: Request, res: Response, next: NextFunction) => {
   const header = req.headers["authorization"];
@@ -16,6 +17,49 @@ export const auth = (req: Request, res: Response, next: NextFunction) => {
   jwt.verify(token, process.env.JWT_SECRET, (err, payload) => {
     if (err) {
       return res.status(403).json({ message: "Vous n'êtes pas connecté." });
+    }
+
+    if (
+      typeof payload === "object" &&
+      payload !== null &&
+      "userId" in payload &&
+      "role" in payload
+    ) {
+      req.user = {
+        userId: (payload as any).userId,
+        role: (payload as any).role,
+      };
+      next();
+    } else {
+      return res.status(403).json({ message: "Token invalide." });
+    }
+  });
+};
+
+export const authAdmin = (req: Request, res: Response, next: NextFunction) => {
+  const header = req.headers["authorization"];
+  if (!header || !header.startsWith("Bearer ")) {
+    return res.status(401).json({ message: "Vous n'êtes pas connecté." });
+  }
+
+  const token = header.split(" ")[1];
+
+  if (!process.env.JWT_SECRET) {
+    throw new Error("JWT_SECRET is missing");
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, payload) => {
+    if (err) {
+      return res.status(403).json({ message: "Vous n'êtes pas connecté." });
+    }
+
+    if (
+      typeof payload === "object" &&
+      payload !== null &&
+      "userId" in payload &&
+      "role" in payload && (payload as any).role !== UserRole.ADMIN
+    ) {
+      return res.status(403).json({ message: "Accès refusé." });
     }
 
     if (
